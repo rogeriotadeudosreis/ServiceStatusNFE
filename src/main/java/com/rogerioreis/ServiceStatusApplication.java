1e4ca897a21d27ed578846f9f0cf7973fd4cb567
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2022-05-26 11:02:32 -0300

    Start do projeto
    Criado os pacotes principais, arquivo de script, configuração Postgress e incluída a dependência do jsoup

---
+++ src/main/java/com/rogerioreis/ServiceStatusApplication.java
@@ -0,0 +1,13 @@
+package com.rogerioreis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerioreis/ServiceStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerioreis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
